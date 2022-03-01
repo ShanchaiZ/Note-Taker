95272bb41cb9173bce6d7db0dd787a114872afbb
ShanchaiZ <shanchai.zahid@gmail.com>
2022-02-28 22:05:04 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Note-Taker
+
+# Description:
+
+An application to write and save note with Express.js back end and will save and retrieve note data from JSON file.
+
+# Usage:
+
+- Application allows user to add a note title and description
+- Application allows user to save notes for future reference. Notes will persist after refreshing page.
+- Application allows user to delete notes
+
+# Technologies
+
+HTML
+CSS
+Javascript:
+Node.js
+Express.js
+Npm
+
+# Deployed Link:
+
+
---
+++ server.js
@@ -0,0 +1,108 @@
+//Install the following Dependencies
+let express = require("express");
+let path = require("path");
+let fs = require("fs")
+
+
+// Sets up the Express App
+let app = express();
+let PORT = process.env.PORT || 3001; 
+//this is the port number that Heroku will be using
+
+// Sets up the Express app to handle data parsing.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// HTML Routes
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+
+});
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    let id = notes[notes.length - 1].id + 1
+    let newNote = { title: req.body.title, text: req.body.text, id: id }
+    let activeNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      if (error) {
+        return error
+      }
+      console.log(activeNote)
+      res.json(activeNote);
+    })
+  })
+})
+
+// Pull the notes from db.json
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log("This is Notes", data)
+    res.json(JSON.parse(data))
+  })
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
+
+
+
+//Put the updated data to db.json
+app.put("/api/notes/:id", function(req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
+    if (error ){
+      return console.log(error)
+    }
+    notes.JSONparse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+    
+    //write the updated data to db.json and display the updated note
+    fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
+
+// Server Start
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
